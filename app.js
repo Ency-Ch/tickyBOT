0b318868fca65220afbb727062de83b0a7f1d04d
ency ch <encee07@yahoo.co.uk>
2021-07-15 02:25:22 +0100

    font chaged to sigika (google), javascript changes on the scroll button. button now appear after scrolling 80% of the window instead of close to 100%

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
- const scrolly = document.getElementById('up_button')
 
- scrolly.addEventListener('click', scrollUp)
+var up_button = document.querySelector('.up_button')
+
+var rootElement = document.documentElement
+
+document.addEventListener('scroll', handleScroll)
+
+
+ up_button.addEventListener('click', scrollUp)
 
 function scrollUp(){
     window.scrollTo(
         {top: 0,
         left:0,
         behaviour: 'smooth'})
-   
-
 }
 
+
+
+function handleScroll (){
+
+    var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
+
+    if ((rootElement.scrollTop / scrollTotal ) > 0.5 ) {
+        // Show button
+        up_button.classList.add("showBtn")
+      } else {
+        // Hide button
+        up_button.classList.remove("showBtn")
+      }
+      console.log('hello')
+}
+
+console.log(up_button.classList)
+console.log(up_button)
